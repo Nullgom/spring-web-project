0608227587c9fa377385c96d40fd966b9a5a6e0c
Codingbear <surfwon@gmail.com>
2021-06-05 16:34:50 +0900

    03 스프링과 Orcle Database 연동

--- jex00/src/main/java/com/codingbear/config/RootConfig.java
+++ jex00/src/main/java/com/codingbear/config/RootConfig.java
@@ -1,10 +1,27 @@
 package com.codingbear.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 @ComponentScan(basePackages = {"com.codingbear.sample"})
 public class RootConfig {
+	@Bean
+	public DataSource dataSource() {
+		HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setDriverClassName("oracle.jdbc.OracleDriver");
+		hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:XE");
+		hikariConfig.setUsername("book_ex");
+		hikariConfig.setPassword("bookex123");
+		
+		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+		return dataSource;
+	}
 
 }
