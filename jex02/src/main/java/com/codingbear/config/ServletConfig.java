66648a05f479ab7c7b818205f99ee699257c3b8f
Codingbear <surfwon@gmail.com>
2021-06-06 19:32:27 +0900

    10 프리젠테이션웹 계층의 CRUD 구현

--- jex02/src/main/java/com/codingbear/config/ServletConfig.java
+++ jex02/src/main/java/com/codingbear/config/ServletConfig.java
@@ -1,11 +1,15 @@
 package com.codingbear.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.codingbear.controller"})
 public class ServletConfig implements WebMvcConfigurer {
 	
 	@Override
---
+++ jex02/src/test/java/com/codingbear/controller/BoardControllerTests.java
@@ -0,0 +1,71 @@
+package com.codingbear.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration // 컨트롤러 테스트 위한 어노테이션(ServletContext 사용하기 위해)
+@ContextConfiguration(classes = { com.codingbear.config.RootConfig.class, com.codingbear.config.ServletConfig.class })
+@Log4j
+public class BoardControllerTests {
+	@Setter(onMethod_ = @Autowired)
+	private WebApplicationContext ctx;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build(); // 가짜 mvc 만들기
+	}
+
+	@Test // 목록에 대한 처리와 테스트
+	public void testList() throws Exception {
+		log.info(
+				mockMvc.perform(MockMvcRequestBuilders.get("/board/list")).andReturn().getModelAndView().getModelMap());
+	}
+
+	@Test // 등록 처리와 테스트
+	public void testRegister() throws Exception {
+		String resultPage = mockMvc
+				.perform(MockMvcRequestBuilders.post("/board/register").param("title", "테스트 새글 제목")
+						.param("content", "테스트 새글 내용").param("writer", "user00"))
+				.andReturn().getModelAndView().getViewName();
+
+		log.info(resultPage);
+	}
+
+	@Test // 조회 처리와 테스트
+	public void testGet() throws Exception {
+		log.info(mockMvc.perform(MockMvcRequestBuilders.get("/board/get").param("bno", "2")).andReturn()
+				.getModelAndView().getModelMap());
+	}
+
+	@Test // 수정 처리와 테스트
+	public void testModify() throws Exception {
+		String resultPage = mockMvc
+				.perform(MockMvcRequestBuilders.post("/board/modify").param("bno", "1", null)
+						.param("title", "수정된 테스트 새글 제목").param("content", "수정된 테스트 새글 내용").param("writer", "user00"))
+				.andReturn().getModelAndView().getViewName();
+
+		log.info(resultPage);
+	}
+
+	@Test // 삭제 처리와 테스트
+	public void testRemove() throws Exception {
+		// 삭제전 데이터베이스에 게시물 번호 확인할 것
+		String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/remove").param("bno", "14")).andReturn()
+				.getModelAndView().getViewName();
+		log.info(resultPage);
+	}
+}
